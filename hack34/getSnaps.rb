a209681fbde7e3179cee30c4a55f4a6a7f65edd7
Eric Fung <efung@acm.org>
2012-11-09 21:55:31 -0500

    Refactor into a class

--- hack34/getSnaps.rb
+++ hack34/getSnaps.rb
@@ -14,91 +14,109 @@ require 'open-uri'
 require 'optparse'
 require 'timeout'
 
-options = {}
-
-opt_parser = OptionParser.new do |opt|
-  opt.banner= <<-eob
-getSnaps.pl [--big] [--partition] <photolist_file> [dirname]
-  eob
-  opt.summary_indent = ''
-  opt.summary_width = 16
-
-  opt.on('--big', 'Return medium-sized photos instead of thumbnails') do
-    options[:big] = true
+class GetSnaps
+  def initialize(args)
+    @ARGV = args
   end
 
-  opt.on('--partition', 'Save photos in folders grouped by server number') do
-    options[:partition] = true
-  end
+  def parse_opts
+    @options = {}
 
-  opt.on('--verbose', 'Dump verbose information') do
-    options[:verbose] = true
-  end
-end
+    opt_parser = OptionParser.new do |opt|
+      opt.banner= <<-eob
+getSnaps.rb [--big] [--partition] <photolist_file> [dirname]
+      eob
+      opt.summary_indent = ''
+      opt.summary_width = 16
 
-opt_parser.parse!
-
-if ARGV.length == 0 then
-  puts opt_parser
-  exit
-end
-
-photolist = ARGV.shift
-dirname = ARGV.shift
-
-dirname = photolist if !dirname
-dirname = dirname.sub(/\.rb$/, '')
-photolist += '.rb' if !(photolist =~ /\./)
-
-require "./#{photolist}"
-
-abort "#{photolist} does not define PHOTOS" if !defined?(PHOTOS)
-
-nbrAdded = 0
-
-if !File.directory?(dirname)
-  puts "Making directory #{dirname}"
-  Dir.mkdir(dirname)
-end
-
-suffix = options[:big] ? '' : '_t'
-
-PHOTOS.each do |photo|
-  # The FlickRaw URL helper methods expect accessor methods
-  def photo.method_missing(*args)
-    self[args[0].to_s]
-  end
-  purlt = options[:big] ? FlickRaw::url(photo) : FlickRaw::url_t(photo)
-  if !options[:partition]
-    fname = "#{dirname}/#{photo['id']}#{suffix}.jpg"
-  else
-    subdirname = "#{dirname}/server#{photo['server']}/#{photo['id'].to_i / 1000}"
-    FileUtils::mkdir_p subdirname
-    fname = "#{subdirname}/#{photo['id']}#{suffix}.jpg"
-  end
-  puts "Checking #{fname}..." if options[:verbose]
-
-  next if File.exists?(fname)
-
-  puts "Adding #{fname}"
-
-  retries = 5
-  begin
-    Timeout::timeout(5) {
-      open(purlt) do |url|
-        File.open(fname, 'wb') do |f|
-          f.puts url.read
-        end
+      opt.on('--big', 'Return medium-sized photos instead of thumbnails') do
+        @options[:big] = true
       end
-      puts "#{nbrAdded}..." if ((nbrAdded += 1) % 50 == 0)
-    }
-  rescue Timeout::Error
-    if (retries -= 1) > 0
-      sleep 1 and retry
-    else
-      raise "Couldn't get image #{purlt}"
+
+      opt.on('--partition', 'Save photos in folders grouped by server number') do
+        @options[:partition] = true
+      end
+
+      opt.on('--verbose', 'Dump verbose information') do
+        @options[:verbose] = true
+      end
+    end
+
+    opt_parser.parse!
+
+    if ARGV.length < 1 then
+      puts opt_parser
+      exit
     end
   end
+
+  def run
+    photolist = @ARGV.shift
+    dirname = @ARGV.shift
+
+    dirname = photolist if !dirname
+    dirname = dirname.sub(/\.rb$/, '')
+    photolist += '.rb' if !(photolist =~ /\./)
+
+    require "./#{photolist}"
+
+    abort "#{photolist} does not define PHOTOS" if !defined?(PHOTOS)
+
+    nbrAdded = 0
+
+    if !File.directory?(dirname)
+      puts "Making directory #{dirname}"
+      Dir.mkdir(dirname)
+    end
+
+    suffix = @options[:big] ? '' : '_t'
+
+    PHOTOS.each do |photo|
+      # The FlickRaw URL helper methods expect accessor methods
+      def photo.method_missing(*args)
+        self[args[0].to_s]
+      end
+      purlt = @options[:big] ? FlickRaw::url(photo) : FlickRaw::url_t(photo)
+      if !@options[:partition]
+        fname = "#{dirname}/#{photo['id']}#{suffix}.jpg"
+      else
+        subdirname = "#{dirname}/server#{photo['server']}/#{photo['id'].to_i / 1000}"
+        FileUtils::mkdir_p subdirname
+        fname = "#{subdirname}/#{photo['id']}#{suffix}.jpg"
+      end
+      puts "Checking #{fname}..." if @options[:verbose]
+
+      next if File.exists?(fname)
+
+      puts "Adding #{fname}"
+
+      retries = 5
+      begin
+        Timeout::timeout(5) {
+          open(purlt) do |url|
+            File.open(fname, 'wb') do |f|
+              f.puts url.read
+            end
+          end
+          puts "#{nbrAdded}..." if ((nbrAdded += 1) % 50 == 0)
+        }
+      rescue Timeout::Error
+        if (retries -= 1) > 0
+          sleep 1 and retry
+        else
+          raise "Couldn't get image #{purlt}"
+        end
+      end
+    end
+
+    puts "#{nbrAdded} added"
+  end
 end
 
-puts "#{nbrAdded} added"
+# main
+if __FILE__ == $PROGRAM_NAME
+  c = GetSnaps.new(ARGV)
+  c.parse_opts
+  c.run
+end
+
